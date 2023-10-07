ed42ebcf602a135722db92583c06393d6d7aa46e
thaitdth <thaitdth1998@gmail.com>
2023-10-08 03:16:35 +0700

    Second

--- DemoConsole/Bai18.cs
+++ DemoConsole/Bai18.cs
@@ -10,7 +10,24 @@ namespace DemoConsole
     {
         public void Run()
         {
-            Console.WriteLine("Hello World");
+            string[] classOne = { "Allan", "Chris", "Monica" };
+            string[] classTwo = {"Katie", "Niel", "Mark"};
+            Console.WriteLine("Student of Class I\tStudent of Class II");
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine(classOne[i] + "\t\t\t" + classTwo[i]);
+            Console.WriteLine("Test1");
+            classTwo = classOne;
+            Console.WriteLine("\nStudent of Class II after referencing Class I:");
+            for (int i = 0; i < 3; i++)
+                Console.Write(classTwo[i] + " ");
+            Console.WriteLine();
+            Console.WriteLine();
+            classOne[1] = "Ben";
+            classTwo[2] = "Mike";
+            Console.WriteLine("Students of Class I after changing the third student of Class II");
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine(classOne[i] + "\t" + classTwo[i]);
+            Console.WriteLine("Test2");
         }
     }
 }
