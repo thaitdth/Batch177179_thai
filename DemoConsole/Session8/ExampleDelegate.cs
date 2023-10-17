5b9d814bda53dfb813725d3e4a6849a2d06f2a45
thaitdth <thaitdth1998@gmail.com>
2023-10-17 20:42:45 +0700

    Session 8 6

---
+++ DemoConsole/Session8/ExampleDelegate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole.Session8
+{
+	public delegate int MyDelegate(int num1, int num2);
+	public class ExampleDelegate
+	{
+		private int Add(int num1, int num2) => num1 + num2;
+		private int Sub(int num1, int num2) => num1 - num2;
+		public void Run()
+		{
+			int a = 10, b = 20;
+			int result;
+			//Set target method
+			MyDelegate obj1 = new MyDelegate(Add);
+			//Invoke method
+			result = obj1(a, b);
+			Console.WriteLine($"{a} + {b} = {result}");
+		}
+	}
+}
