702528c1a57408e4ef68e3a39e36c33403a4df40
thaitdth <thaitdth1998@gmail.com>
2023-10-17 21:09:12 +0700

    Session 8 7

--- DemoConsole/Session8/ExampleDelegate.cs
+++ DemoConsole/Session8/ExampleDelegate.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace DemoConsole.Session8
 {
+	//Create Delegate
 	public delegate int MyDelegate(int num1, int num2);
+	public delegate void MyDelegate2(string msg);
+	class MyClass
+	{
+		public void PrintUpper(string mess) => Console.WriteLine(mess.ToUpper());
+		public void PrintLower(string mess) => Console.WriteLine(mess.ToLower());
+	}
 	public class ExampleDelegate
 	{
+		private void InvokeDelegate(MyDelegate2 dele, string msg) => dele(msg);
 		private int Add(int num1, int num2) => num1 + num2;
 		private int Sub(int num1, int num2) => num1 - num2;
 		public void Run()
@@ -20,6 +28,10 @@ namespace DemoConsole.Session8
 			//Invoke method
 			result = obj1(a, b);
 			Console.WriteLine($"{a} + {b} = {result}");
+			string msg = "This is a message.";
+			MyClass obj2 = new MyClass();
+			InvokeDelegate(obj2.PrintUpper, msg);
+			InvokeDelegate(obj2.PrintLower, msg);
 		}
 	}
 }
