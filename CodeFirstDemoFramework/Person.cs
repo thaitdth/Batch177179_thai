6b9180be6fbdbbaaf77506ad07113b3edacf3ba8
thaitdth <thaitdth1998@gmail.com>
2023-10-29 00:44:14 +0700

    Database Connection

---
+++ CodeFirstDemoFramework/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace CodeFirstDemoFrameWork
+{
+	public class Person
+	{
+		public int PersonId { get; set; }
+		[Required]
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+	}
+}
---
+++ CodeFirstDemoFramework/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstDemoFrameWork;
+
+namespace CodeFirstDemoFramework
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var context = new Context())
+			{
+				context.Database.CreateIfNotExists();
+				var person = new Person
+				{
+					FirstName = "DOTNET",
+					LastName = "GROUP"
+				};
+				context.People.Add(person);
+				context.SaveChanges();
+			}
+		}
+	}
+}
